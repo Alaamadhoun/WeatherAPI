31e5cb0d3300ed3ab4ea9f023ea8e849c71c50c7
Alaamadhoun <alaafm@live.com>
2019-01-03 00:40:41 +0800

    Form Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 class App extends Component {
+  getWeather = e => {
+    e.preventDefault();
+    console.log("Weather");
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
     );
   }
