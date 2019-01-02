c6d8c2ff72c112647b897b3397451ec2e5e30eeb
Alaamadhoun <alaafm@live.com>
2019-01-03 02:35:52 +0800

    CSS Finalized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
+import Titles from "./components/Titles";
 
 const API_KEY = "528d30ed7c37939379e836b4798a0dab";
 //https://api.openweathermap.org/data/2.5/weather?q=Kuala%20Lumpur,my&appid=528d30ed7c37939379e836b4798a0dab
@@ -46,7 +47,7 @@ class App extends Component {
     }
   };
 
-  render() {
+  /*  render() {
     return (
       <div className="App">
         <Form getWeather={this.getWeather} />
@@ -58,6 +59,36 @@ class App extends Component {
           description={this.state.description}
           error={this.state.error}
         />
+      </div>
+    );
+  }
+}*/
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,36 +1,40 @@
 import React from "react";
 
-const Weather = props => {
-  return (
-    <div>
-      {props.temperature && (
-        <p>
-          <strong>Temperature: </strong> {props.temperature}
-        </p>
-      )}
-      {props.city && (
-        <p>
-          <strong>City:</strong> {props.city}
-        </p>
-      )}
-      {props.country && (
-        <p>
-          <strong>Country:</strong> {props.country}
-        </p>
-      )}
-      {props.humidity && (
-        <p>
-          <strong>Humidity:</strong> {props.humidity}
-        </p>
-      )}
-      {props.description && (
-        <p>
-          <strong>Description: </strong>
-          {props.description}
-        </p>
-      )}
-    </div>
-  );
-};
+const Weather = props => (
+  <div className="weather__info">
+    {props.city && props.country && (
+      <p className="weather__key">
+        {" "}
+        Location:
+        <span className="weather__value">
+          {" "}
+          {props.city}, {props.country}
+        </span>
+      </p>
+    )}
+    {props.temperature && (
+      <p className="weather__key">
+        {" "}
+        Temperature:
+        <span className="weather__value"> {props.temperature} </span>
+      </p>
+    )}
+    {props.humidity && (
+      <p className="weather__key">
+        {" "}
+        Humidity:
+        <span className="weather__value"> {props.humidity} </span>
+      </p>
+    )}
+    {props.description && (
+      <p className="weather__key">
+        {" "}
+        Conditions:
+        <span className="weather__value"> {props.description} </span>
+      </p>
+    )}
+    {props.error && <p className="weather__error">{props.error}</p>}
+  </div>
+);
 
 export default Weather;
